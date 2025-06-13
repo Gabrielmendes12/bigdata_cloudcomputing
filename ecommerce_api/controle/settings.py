53012cea56336a129ed79121ab5ed02dba91f8d1
Gabrielmendes12 <gabrielmendes0102@gmail.com>
2025-06-13 04:20:00 -0300

    controle

--- ecommerce_api/controle/settings.py
+++ ecommerce_api/controle/settings.py
@@ -73,7 +73,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "config.urls"
+ROOT_URLCONF = "controle.urls"
 
 TEMPLATES = [
     {
@@ -91,7 +91,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "config.wsgi.application"
+WSGI_APPLICATION = "controle.wsgi.application"
 
 
 # Database
